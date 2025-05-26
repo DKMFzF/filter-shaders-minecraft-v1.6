c9ec011831f4e17f0d3aa13b7548347ac0288c6c
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-26 21:26:06 +0700

    feat: added settings.glsl

---
+++ shaders/settings.glsl
@@ -0,0 +1,22 @@
+// buffers
+#define SKY_DARK 0 // [0 1]
+
+// colors
+#define BLUE_AMOUNT 0.25 //[0.0 0.25 0.5 0.75 1.0]
+#define GREEN_AMOUNT 0.25 //[0.0 0.25 0.5 0.75 1.0]
+#define GRAY_AMOUNT 0.25 //[0.0 0.25 0.5 0.75 1.0]
+#define RED_AMOUNT 0.25 //[0.0 0.25 0.5 0.75 1.0]
+
+// effects
+#define EFFECT_SEPIA 0 //[0 1]
+#define EFFECT_INVERT 0 //[0 1]
+#define EFFECT_OLD_FILM 0 //[0 1]
+#define EFFECT_CONTRAST 0.0 //[0.0 0.25 0.5 0.75 1.0]
+#define EFFECT_BRIGHTNESS 0.0 //[0.0 0.25 0.5 0.75 1.0]
+#define EFFECT_SATURATION 0.0 //[0.0 0.25 0.5 0.75 1.0]
+
+// blur effect
+#define EFFECT_BLUR 0.0 //[0.0 0.25 0.5 0.75 1.0]
+#define EFFECT_BLUR_RADIUS 0.002 // [0.001 0.002 0.003 0.005]
+#define BLUR_CENTER_RADIUS 0.2 // [0.1 0.2 0.3 0.4 0.5]
+#define BLUR_TRANSITION 0.1 // [0.05 0.1 0.15]
